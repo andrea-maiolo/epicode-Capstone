5c606c93a2b3025f15d9da659c9b1870d79fe3af
Andrea Maiolo <maiolo.m.2@gmail.com>
2025-09-05 16:52:47 +0200

    made basic admin page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
+import AdminPage from "./pages/AdminPage";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/admin" element={<AdminPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/pages/AdminPage.jsx
@@ -0,0 +1,63 @@
+import { Col, Container, Nav, Row } from "react-bootstrap";
+
+const AdminPage = function () {
+  return (
+    <>
+      <Nav>gestisci stanze, gestisci ospiti</Nav>
+      <Container>
+        <Row>
+          <Col>rooms</Col>
+          <Col>day1</Col>
+          <Col>d2</Col>
+          <Col>d3</Col>
+          <Col>d4</Col>
+          <Col>d5</Col>
+          <Col>d6</Col>
+          <Col>d7</Col>
+        </Row>
+        <Row>
+          <Col>rooms</Col>
+          <Col>day1</Col>
+          <Col>d2</Col>
+          <Col>d3</Col>
+          <Col>d4</Col>
+          <Col>d5</Col>
+          <Col>d6</Col>
+          <Col>d7</Col>
+        </Row>
+        <Row>
+          <Col>rooms</Col>
+          <Col>day1</Col>
+          <Col>d2</Col>
+          <Col>d3</Col>
+          <Col>d4</Col>
+          <Col>d5</Col>
+          <Col>d6</Col>
+          <Col>d7</Col>
+        </Row>
+        <Row>
+          <Col>rooms</Col>
+          <Col>day1</Col>
+          <Col>d2</Col>
+          <Col>d3</Col>
+          <Col>d4</Col>
+          <Col>d5</Col>
+          <Col>d6</Col>
+          <Col>d7</Col>
+        </Row>
+        <Row>
+          <Col>rooms</Col>
+          <Col>day1</Col>
+          <Col>d2</Col>
+          <Col>d3</Col>
+          <Col>d4</Col>
+          <Col>d5</Col>
+          <Col>d6</Col>
+          <Col>d7</Col>
+        </Row>
+      </Container>
+    </>
+  );
+};
+
+export default AdminPage;
