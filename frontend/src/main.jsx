d00d436d0664812314d041effc283983261fb69a
Andrea Maiolo <maiolo.m.2@gmail.com>
2025-09-05 11:58:35 +0200

    added sass and working on using it

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,8 @@
 import { createRoot } from "react-dom/client";
+import "./App.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
-//import "./index.css";
+
+import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(<App />);
